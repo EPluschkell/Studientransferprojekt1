6e6b66a332c8695bf4b5fbb35a9d71daadafbac1
JoeyDanz <149071204+JoeyDanz@users.noreply.github.com>
2023-10-26 16:57:18 +0200

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,38 @@
 import java.time.LocalDate;
-import java.util.Date;
 
 public class Main {
-    public static class Essen {
-        String EssenName;
-        String EssenAblaufdatum;
-        int EssenID;
-        public Essen(String Name, String Datum ){
-            EssenName = Name;
-            EssenAblaufdatum = Datum;
-            EssenID = 1;
+    public static class Lebensmittel {
+        //int id;
+        String LebensmittelName;
+        LocalDate MHD;
+        Boolean angebrochen;
+        int menge;
+        Boolean gml; // true = g false = ml
+        Boolean mhdueberschritten; // true = ist abgelaufen
+        public Lebensmittel(String Name, LocalDate Datum, Boolean Angebrochen, int Menge, Boolean Einheit){
+            LebensmittelName = Name;
+            MHD = Datum;
+            angebrochen = Angebrochen;
+            menge = Menge;
+            gml = Einheit;
+            mhdueberschritten = !MHD.isAfter(LocalDate.now());
+            //id = 1;
         }
+
     }
     public static void main(String[] args) {
-        System.out.println(LocalDate.now());
-        System.out.println("Test!");
-        Essen Fleisch = new Essen("Rinderhack", "2023-10-30");
+        String name1 = "Rinderhack";
+        LocalDate datum1 = LocalDate.of(2023,10,30);
+        Boolean angebrochen1 = false;
+        int menge1 = 500;
+        boolean einheit1 = true; //gramm
 
-        System.out.println(Fleisch.EssenAblaufdatum+" "+Fleisch.EssenName);
+        Lebensmittel Fleisch = new Lebensmittel(name1,datum1,angebrochen1,menge1,einheit1);
+        if(Fleisch.mhdueberschritten){
+            System.out.println("Das Lebensmittel " + Fleisch.LebensmittelName + " ist abgelaufen");
+        } else{
+            System.out.println("Das Lebensmittel " + Fleisch.LebensmittelName + " ist noch " + datum1.until(LocalDate.now()) + " haltbar.");
+        }
+
     }
 }
