ae8013871917074dd1ff6409122dec37458aca08
eplus <e.pluschkell@gmail.com>
2023-10-26 14:43:46 +0200

    Anfang

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.time.LocalDate;
+
 public class Main {
+    public class Essen {
+        String EssenName;
+        String EssenAblaufdatum;
+        int EssenID;
+        public Essen(String Name, String Datum ){
+            EssenName = Name;
+            EssenAblaufdatum = Datum;
+            EssenID = 1;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(LocalDate.now());
         System.out.println("Test!");
     }
 }
