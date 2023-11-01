8fe9416893c8f6e2aa627909760a25e9e7b7bef6
eplus <e.pluschkell@gmail.com>
2023-11-01 16:37:54 +0100

    Implementierung der Lebensmittel Klasse in der GUI

--- src/testtest.java
+++ src/testtest.java
@@ -1,7 +1,5 @@
 package src;
 
-import java.awt.*;        // Using AWT container and component classes
-import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.swing.*;
@@ -34,9 +32,6 @@ public class testtest {
             gml = false;
             mhdueberschritten = !MHD.isAfter(LocalDate.now());
         }
-        public void setMHD(int Tag,int Monat,int Jahr){
-            MHD = LocalDate.of(Jahr,Monat,Tag);
-        }
 
         public Object[] toArray(){
             int timeLeft = MHD.compareTo(LocalDate.now());
@@ -49,9 +44,10 @@ public class testtest {
             Object[] object = new Object[6];
             object[0]= LebensmittelName;
             object[1]=""+menge+einheit;
-            object[3]=MHD;
-            object[4]=timeLeft;
-            object[5]=angebrochen;
+            object[2]=MHD;
+            object[3]=timeLeft;
+            object[4]=angebrochen;
+            object[5]=mhdueberschritten;
             return object;
         }
         @Override
@@ -73,31 +69,7 @@ public class testtest {
         }
 
     }
-    public static class Essen {
-        String EssenName;
-        String EssenAblaufdatum;
 
-        public Essen(String Name, String Datum){
-            EssenName = Name;
-            EssenAblaufdatum = Datum;
-
-        }
-        public Object[] toArray(){
-            Object[] object = new Object[2];
-            object[0]= EssenName;
-            object[1]=EssenAblaufdatum;
-            return object;
-        }
-        @Override
-        public String toString() {
-            return EssenName + ':' + EssenAblaufdatum ;
-        }
-    }
-   /* private TextField inputName;
-    private TextField inputDate;
-    private TextArea listText;
-    private Button btn;// Declare a Button component*/
-    private JTextArea listText;
     private JTextField inputName;
     private JTextField inputDay;
     private JTextField inputMonth;
@@ -106,69 +78,44 @@ public class testtest {
     private JButton btn;
     private JToggleButton btnAngebr;
     private JToggleButton btnGML;
-    public static ArrayList<Essen> list = new ArrayList<>();
     public ArrayList<Lebensmittel> lebensmittelList = new ArrayList<>();
-    String[] columnNames = {"Lebensmittel","Menge","Ablaufdatum","Angebrochen?","Abgelaufen?"};
-    //String[] columnNames = {"Lebensmittel","Ablaufdatum"};
+    String[] columnNames = {"Lebensmittel","Menge","Ablaufdatum","Zeit Übrig","Angebrochen?","Abgelaufen?"};
 
     // Constructor to setup GUI components and event handlers
     public testtest () {
-       /* setLayout(new FlowLayout());
-        // "super" Frame, which is a Container, sets its layout to FlowLayout to arrange
-        // the components from left-to-right, and flow to next row from top-to-bottom.
-
-
-        inputName = new TextField(30);
-        inputName.setEditable(true);
-        add(inputName);
-        inputDate=new TextField(30);
-        inputDate.setEditable(true);
-        add(inputDate);
-        btn = new Button("enter");
-        add(btn);
-        listText = new TextArea();
-        listText.setEditable(false);
-        add(listText);
-
-
-        btnListener listener = new btnListener();
-        btn.addActionListener(listener);
-
-        setTitle("Kühlschrank");  // "super" Frame sets its title
-        setSize(600, 300);        // "super" Frame sets its initial window size
-
-        // For inspecting the Container/Components objects
-        setVisible(true);         // "super" Frame shows*/
-
-        /*Object[][] data = new Object[list.size()+100][list.getFirst().toArray().length];
-        for(int i =0;i<list.size();i++){
-            data[i]= list.get(i).toArray();
-        }*/
-
 
         JFrame frame = new JFrame("Test Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        frame.setSize(450, 400);
         JPanel panel = new JPanel();
-        inputName = new JTextField(15);
 
+        JLabel label1 = new JLabel("Name:");
+        panel.add(label1);
+        inputName = new JTextField(8);
         panel.add(inputName);
-        inputDay = new JTextField(10);
+        JLabel label2 = new JLabel("Tag:");
+        panel.add(label2);
+        inputDay = new JTextField(4);
         panel.add(inputDay);
-        inputMonth = new JTextField(5);
+        JLabel label3 = new JLabel("Monat:");
+        panel.add(label3);
+        inputMonth = new JTextField(4);
         panel.add(inputMonth);
+        JLabel label4 = new JLabel("Jahr:");
+        panel.add(label4);
         inputYear = new JTextField(5);
         panel.add(inputYear);
-        inputAmount = new JTextField(8);
+        JLabel label5 = new JLabel("Menge:");
+        panel.add(label5);
+        inputAmount = new JTextField(7);
         panel.add(inputAmount);
-        btnGML = new JToggleButton("Flüssigkeit?");
+        btnGML = new JToggleButton("In g oder ml?");
         panel.add(btnGML);
         btnAngebr=new JToggleButton("Angebrochen?");
         panel.add(btnAngebr);
         btn = new JButton("Enter");
         panel.add(btn);
 
-        JPanel panel1 = new JPanel();
         JTable table = new JTable();
         DefaultTableModel dtm = new DefaultTableModel(0,0);
         dtm.setColumnIdentifiers(columnNames);
@@ -178,10 +125,7 @@ public class testtest {
         }
 
 
-        listText = new JTextArea();
-        listText.setEditable(false);
-        panel1.add(table);
-        JScrollPane scrollPane = new JScrollPane(panel1,ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        JScrollPane scrollPane = new JScrollPane(table);
 
         JTabbedPane tabpane = new JTabbedPane();
         tabpane.add("input",panel);
@@ -189,58 +133,26 @@ public class testtest {
 
 
         frame.getContentPane().add(tabpane);
-        //frame.getContentPane().add(BorderLayout.SOUTH, panel1);
         frame.setVisible(true);
 
         btn.addActionListener(e -> {
-            listText.setText("");
             Lebensmittel placeholder;
             placeholder = new Lebensmittel(inputName.getText(),LocalDate.of(Integer.parseInt(inputYear.getText()),Integer.parseInt(inputMonth.getText()),Integer.parseInt(inputDay.getText())),btnAngebr.isSelected(),Integer.parseInt(inputAmount.getText()),btnGML.isSelected());
-            Essen essenPlaceholder;
             lebensmittelList.add(placeholder);
-            essenPlaceholder = new Essen(inputName.getText(), inputDay.getText());
-            list.add(essenPlaceholder);
-            String textPlaceholder="";
-            //listText.setText(list.toString());
             dtm.addRow(lebensmittelList.get(lebensmittelList.size()-1).toArray());
-            for(int i=0;i<list.size();i++){
-                //list.get(i).toString();
-                //listText.setText((list.get(i)).toString());
-                listText.append((list.get(i)).toString()+"\n");
-
-            }
+            inputName.setText("");
+            inputMonth.setText("");
+            inputDay.setText("");
+            inputYear.setText("");
+            inputAmount.setText("");
         });
     }
 
     // The entry main() method
     public static void main(String[] args) {
-        Essen brot = new Essen("brot", "23.11.23");
-        Essen fleisch = new Essen("Fleisch","08.11.23");
-        Essen Sauce = new Essen("Sauce", "05.11.23");
-        Essen Kartoffeln = new Essen("Kartoffeln", "30.11.23");
-        list.add(brot);
-        list.add(fleisch);
-        list.add(Sauce);
-        list.add(Kartoffeln);
         // Invoke the constructor to setup the GUI, by allocating an instance
         testtest app = new testtest();
     }
 
-    /*private class btnListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent evt) {
-            listText.setText("");
-            Essen placeholder;
-            placeholder = new Essen(inputName.getText(),inputDate.getText());
-            list.add(placeholder);
-            String textPlaceholder="";
-            //listText.setText(list.toString());
-            for(int i=0;i<list.size();i++){
-                //list.get(i).toString();
-                //listText.setText((list.get(i)).toString());
-                listText.append((list.get(i)).toString()+"\n");
-            }
-        }
-    }*/
 }
 
